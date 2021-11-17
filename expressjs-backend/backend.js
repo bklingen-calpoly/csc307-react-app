95008bf0190c2631716df5f04361db5d3b9241c9
BJ Klingenberg <bklingen@calpoly.edu>
2021-11-17 11:24:51 -0800

    add post

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -39,8 +39,45 @@ app.get("/", (req, res) => {
 });
 
 app.get("/users", (req, res) => {
-  res.send(users);
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
 });
+
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined || result.length == 0)
+    res.status(404).send("Resource not found.");
+  else {
+    result = { users_list: result };
+    res.send(result);
+  }
+});
+
+function findUserById(id) {
+  return users["users_list"].find((user) => user["id"] === id); // or line below
+  //return users['users_list'].filter( (user) => user['id'] === id);
+}
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.status(200).end();
+});
+
+function addUser(user) {
+  users["users_list"].push(user);
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
