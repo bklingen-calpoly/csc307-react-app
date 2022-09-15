2c0372729c04a4c8ccca97bf0820cdcaa15ef0ef
BJ Klingenberg <bklingen@calpoly.edu>
2022-09-15 13:55:24 -0700

    update to react 18.2. change root render

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import ReactDOMClient from "react-dom/client";
 import MyApp from "./MyApp";
 import "./index.css";
 
-ReactDOM.render(<MyApp />, document.getElementById("root"));
+// ReactDOM.render(<MyApp />, document.getElementById("root"));
+
+const container = document.getElementById("root");
+// Create a root
+
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render: Render an element to the Root
+root.render(<MyApp />);
