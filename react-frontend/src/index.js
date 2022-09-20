a1b013b980871e352db0a8b6829d67165a36a782
BJ Klingenberg <bklingen@calpoly.edu>
2022-09-20 15:13:20 -0700

    fix create root

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
-// import ReactDOM from "react-dom";
 import ReactDOMClient from "react-dom/client";
 import MyApp from "./MyApp";
 import "./index.css";
-
-// ReactDOM.render(<MyApp />, document.getElementById("root"));
 
 const container = document.getElementById("root");
 // Create a root
