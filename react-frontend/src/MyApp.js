1fd027dc3874712932e9e5d84c990f19e6714d7a
BJ Klingenberg <bklingen@calpoly.edu>
2023-10-04 11:21:45 -0700

    update for demo

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,28 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 import Table from "./Table";
-import Form from "./Form";
-
 // import RedditExample from "./RedditExample";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([]);
-
-  function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
-    });
-    setCharacters(updated);
-  }
-
-  function updateList(person) {
-    setCharacters([...characters, person]);
-  }
+  const characters = [
+    {
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      name: "Dennis",
+      job: "Bartender",
+    },
+    {
+      name: "Alexa",
+      job: "Baker",
+    },
+  ];
 
   return (
     <div className="container">
-      <Table characterData={characters} removeCharacter={removeOneCharacter} />
-      <Form handleSubmit={updateList} />
-      {/* <RedditExample subreddit="calpoly" /> */}
+      <Table characterData={characters} />
+      {/* <RedditExample subreddit="calpoly"/> */}
     </div>
   );
 }
