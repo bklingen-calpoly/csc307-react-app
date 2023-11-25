96422f7288c5ffc759986531744a5e784b2c326b
BJ Klingenberg <75038922+bklingen-calpoly@users.noreply.github.com>
2023-11-25 15:48:55 -0800

    frontend testing final

---
+++ react-frontend/src/Form.test.js
@@ -0,0 +1,47 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Form from "./Form";
+
+test("renders the empty form correctly", () => {
+  render(<Form />);
+
+  expect(screen.getByLabelText("Name")).toBeInTheDocument();
+  expect(screen.getByLabelText("Job")).toBeInTheDocument();
+  expect(screen.getByText("Submit")).toBeInTheDocument();
+});
+
+const testUser = { name: "BJ Klingenberg", job: "Educator" };
+
+test("accepts name input", () => {
+  render(<Form />);
+
+  const input = screen.getByLabelText("Name");
+  fireEvent.change(input, { target: { value: testUser.name } });
+  expect(input).toHaveValue(testUser.name);
+});
+
+test("accepts job input", () => {
+  render(<Form />);
+
+  const input = screen.getByLabelText("Job");
+  fireEvent.change(input, { target: { value: testUser.job } });
+  expect(input).toHaveValue(testUser.job);
+});
+
+test("handles form submission", () => {
+  let formData = {};
+  const mockUpdate = (data) => {
+    formData = data;
+  };
+
+  render(<Form handleSubmit={mockUpdate} />);
+
+  let input = screen.getByLabelText("Name");
+  fireEvent.change(input, { target: { value: testUser.name } });
+  input = screen.getByLabelText("Job");
+  fireEvent.change(input, { target: { value: testUser.job } });
+  const button = screen.getByRole("button", { type: "submit" });
+  fireEvent.click(button);
+
+  expect(formData).toHaveProperty("name", testUser.name);
+  expect(formData).toHaveProperty("job", testUser.job);
+});
