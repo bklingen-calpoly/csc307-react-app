c7c46508354e9e7bc23ed92a10cc89ac09ef18e0
BJ Klingenberg <75038922+bklingen-calpoly@users.noreply.github.com>
2023-11-27 09:18:31 -0800

    udpate base test

--- react-frontend/src/MyApp.test.js
+++ react-frontend/src/MyApp.test.js
@@ -1,9 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import App from "./MyApp";
+import MyApp from "./MyApp";
 import React from "react";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Submit/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders MyApp react link", () => {
+  render(<MyApp />);
+  expect(screen.getByText("Submit")).toBeInTheDocument();
 });
